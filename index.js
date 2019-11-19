c932fedf4bbb6821b55cd527708ad123e9b25540
comical-tag-7436 <shawtynow2008@gmail.com>
2019-11-19 01:11:18 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // add solution here
+function theBeatlesPlay(musician, instruments) {
+  var array = [];
+  for(var i = 0; i < musician.length; i++) {
+    array[i] = (`${musician[i]} plays ${instruments[i]}`);
+  }
+  return array
+}
